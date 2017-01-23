b91bbbc45a3097cb14a1561fa618d99a82c12158
Alexandre Poirot <poirot.alex@gmail.com>
2017-01-23 22:56:24 +0100

    ping travis?

--- build-devtools-xpi.sh
+++ build-devtools-xpi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip -9 -r devtools.xpi . -x "*moz.build" -x "*/test*" -x "*.eslint*" -x "*/docs*" -x docs -x "*.idl" -x "*.cpp" -x "*.h" -x "*.md" -x LICENSE -x README -x shared/heapsnapshot/ -x jar.mn
+zip -9 -r devtools.xpi . -x "*moz.build" -x "*/test*" -x "*.eslint*" -x "*/docs*" -x docs -x "*.idl" -x "*.cpp" -x "*.h" -x "*.md" -x "*LICENSE" -x "*README*" -x "shared/heapsnapshot/*" -x "*jar.mn" -x ".git*" -x .travis.yml -x *.sh -x "gecko*"
